97db4455e233ff82deb8ab80fc75d3cd3b01c191
Alena Burova <attapi343@gmail.com>
2021-12-22 21:42:21 +0300

    Implementations of the basic game of life.

--- app.py
+++ app.py
@@ -1,75 +1,55 @@
 import pygame
-from pygame.locals import *
-from tkinter import *
+from copy import deepcopy
+from numpy import genfromtxt
+
+tile_size = 20
+FPS = 6
+
+current_field = genfromtxt('map.csv', delimiter=',')
+tiles_grid_height, tiles_grid_width = current_field.shape
+RES = screen_width, screen_height = tiles_grid_width * tile_size, tiles_grid_height * tile_size
+
+pygame.init()
+surface = pygame.display.set_mode(RES)
+clock = pygame.time.Clock()
+
+next_field = [[0 for i in range(tiles_grid_width)] for j in range(tiles_grid_height)]
 
 
-class GameOfLife:
-    def __init__(self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10) -> None:
-        self.width = width
-        self.height = height
-        self.cell_size = cell_size
+def check_cell(current_field, x, y):
+    count = 0
+    for j in range(y - 1, y + 2):
+        for i in range(x - 1, x + 2):
+            if current_field[j][i]:
+                count += 1
 
-        # Устанавливаем размер окна
-        self.screen_size = width, height
-        # Создание нового окна
-        self.screen = pygame.display.set_mode(self.screen_size)
-
-        # Вычисляем количество ячеек по вертикали и горизонтали
-        self.cell_width = self.width // self.cell_size
-        self.cell_height = self.height // self.cell_size
-
-        # Скорость протекания игры
-        self.speed = speed
-
-    def draw_lines(self) -> None:
-        for x in range(0, self.width, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color('black'),
-                             (x, 0), (x, self.height))
-        for y in range(0, self.height, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color('black'),
-                             (0, y), (self.width, y))
-
-    def run(self) -> None:
-        pygame.init()
-        clock = pygame.time.Clock()
-        pygame.display.set_caption('Game of Life')
-        self.screen.fill(pygame.Color('white'))
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    running = False
-            self.visualize_grid(matrix_of_elements)
-            self.draw_lines()
-            pygame.display.flip()
-            clock.tick(self.speed)
-        pygame.quit()
-
-    def create_square(self, x, y, color):
-        pygame.draw.rect(self.screen, color, [x, y, self.cell_size, self.cell_size])
-
-    def visualize_grid(self, matrix):
-        y = 0  # we start at the top of the screen
-        for row in matrix:
-            x = 0  # for every row we start at the left of the screen again
-            for item in row:
-                if item == 0:
-                    self.create_square(x, y, pygame.Color('white'))
-                else:
-                    self.create_square(x, y, pygame.Color('green'))
-
-                x += self.cell_size  # for ever item/number in that row we move one "step" to the right
-            y += self.cell_size  # for every new row we move one "step" downwards
-        pygame.display.update()
+    if current_field[y][x]:
+        count -= 1
+        if count == 2 or count == 3:
+            return 1
+        return 0
+    else:
+        if count == 3:
+            return 1
+        return 0
 
 
+while True:
 
-if __name__ == '__main__':
-    game = GameOfLife(320, 240, 20)
+    surface.fill(pygame.Color('black'))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
 
-    matrix_of_elements = [[1, 1, 0, 1],
-                          [1, 0, 0, 1],
-                          [1, 1, 0, 1],
-                          [1, 1, 1, 1]]
+    [pygame.draw.line(surface, pygame.Color('darkslategray'), (x, 0), (x, screen_height)) for x in range(0, screen_width, tile_size)]
+    [pygame.draw.line(surface, pygame.Color('darkslategray'), (0, y), (screen_width, y)) for y in range(0, screen_height, tile_size)]
+    # draw life
+    for x in range(1, tiles_grid_width - 1):
+        for y in range(1, tiles_grid_height - 1):
+            if current_field[y][x]:
+                pygame.draw.rect(surface, pygame.Color('forestgreen'), (x * tile_size + 2, y * tile_size + 2, tile_size - 2, tile_size - 2))
+            next_field[y][x] = check_cell(current_field, x, y)
 
-    game.run()
+    current_field = deepcopy(next_field)
+    pygame.display.flip()
+    clock.tick(FPS)
